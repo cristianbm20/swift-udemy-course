a811c780139edf9c03bdba501473f896bba46b54
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-02-09 13:01:31 +0100

    Day 17: Start section 15: Navigation and passing data between screens

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Cristian Bermudez Martinez on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
