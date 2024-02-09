5efc844e951ea51567e5444145987e9089c022f8
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-02-09 19:23:26 +0100

    Day 17: Complete section 15: Navigation and passing data between screens

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
+  
+  var bgcolor = ""
+  
+  @IBAction func buttonBlack(_ sender: Any) {
+    bgcolor = "black"
+    performSegue(withIdentifier: "vcColor", sender: self)
+  }
+  
+  @IBAction func buttonGreen(_ sender: Any) {
+    bgcolor = "green"
+    performSegue(withIdentifier: "vcColor", sender: self)
+  }
+  
+  @IBAction func buttonPink(_ sender: Any) {
+    bgcolor = "pink"
+    performSegue(withIdentifier: "vcColor", sender: self)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "vcColor" && !bgcolor.isEmpty {
+      if let destiny = segue.destination as? ViewControllerColor {
+        destiny.destinyTitle = bgcolor
+        destiny.destinyColor = bgcolor
+      }
+    }
+  }
+  
 }
-
---
+++ Navigation/ViewControllerGreen.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerGreen.swift
+//  Navigation
+//
+//  Created by Cristian Bermudez Martinez on 9/2/24.
+//
+
+import UIKit
+
+class ViewControllerGreen: UIViewController {
+  
+  var destinyTitle: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  
+}
